33565dd38ceb6b5eb99e101e2ccec9795c818ae6
heavycavalry <klaudia.diana.paluch@gmail.com>
2020-12-11 01:09:05 +0100

    small changes

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ class Flake {
         this.step = Math.random()
         this.x = Math.random() * 1200;
         this.y = Math.random() * 800;
-        this.r = Math.random() * 8;
+        this.r = Math.random() * 5;
+        this.dY = Math.random() * 0.7 + 0.5;
     }
 
     drawFlake() {
@@ -23,21 +24,16 @@ class Flake {
     }
 
 
-
     move() {
         this.step += Math.random() * 0.01;
         this.x = modulo(this.x + Math.sin(this.step), 1200);
-        this.y = modulo(this.y + 0.7, 800);
-
+        this.y = modulo(this.y + this.dY, 800);
+    }
 }
-
-}
-
-
 
 let flakes = [];
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 1000; i++) {
     flakes.push(new Flake());
 }
 
